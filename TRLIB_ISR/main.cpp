b49549c56874c5ace0b72b73d567cd5681a7fb0e
Bouealt <bouealt@gmail.com>
2024-07-07 17:09:37 +0800

    增加Bug记录文件夹
    增加了bug记录文件夹用于添加bug解决方案以及记录

--- TRLIB_ISR/main.cpp
+++ TRLIB_ISR/main.cpp
@@ -9,13 +9,13 @@ InetAddress isrWifiAddr("192.168.3.1", 1234, "isrWifi"); // isr wifi地址
 InetAddress isrLanAddr("192.168.2.1", 2234, "isrLan");	 // isr lan口地址
 
 InetAddress serverAddr("tstit.x3322.net", 12121, "Server"); // 实验室本地服务器地址
-InetAddress aliyunAddr("g2020.top", 8888, "Aliyun");		// 阿里云服务器地址
+InetAddress aliyunAddr("g2020.top", 8888, "Aliyun");		// 阿里云服务器地址g2020.top
 
 int main()
 {
 	EventScheduler *scheduler = EventScheduler::createNew(EventScheduler::POLLER_EPOLL);
 	ThreadPool *threadPool = ThreadPool::createNew(1);
-	std::vector<InetAddress> serverAddrs = {serverAddr};
+	std::vector<InetAddress> serverAddrs = {aliyunAddr};
 	std::unique_ptr<ISRServer> isrServer = ISRServer::createNew(scheduler, threadPool, isrWifiAddr, isrLanAddr, serverAddrs);
 	isrServer->start();
 	scheduler->loop(); // 处理事件
